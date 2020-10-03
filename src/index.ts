29b5929b584ac05fa47c91d1068ebc34c8544df6
spoken-angus <angusjelinek@gmail.com>
2020-10-02 19:06:05 -0500

    added apollo-graphql with initial schema

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,10 @@ import { MikroORM } from "@mikro-orm/core";
 import { __prod__ } from "./constants";
 import { Fridge } from "./entities/Fridge";
 import microConfig from "./mikro-orm.config";
+import express from 'express'
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
+import { HelloResolver } from "./resolvers/hello";
 
 const main = async () => {
   const orm = await MikroORM.init(microConfig);
@@ -9,8 +13,20 @@ const main = async () => {
   // const fridge = orm.em.create(Fridge, { title: "my first post" });
   // await orm.em.persistAndFlush(fridge);
 
-  const fridges = await orm.em.find(Fridge, {});
-  console.log(fridges);
+  const app = express();
+
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver],
+      validate: false
+    }),
+  });
+
+  apolloServer.applyMiddleware({ app });
+
+  app.listen(4000, () => {
+    console.log('server started on localhost:4000')
+  });
 };
 
 main().catch((err) => {
