5d4d1f8a38fde5571f884f9dc2b6a263ca284da1
spoken-angus <angusjelinek@gmail.com>
2020-10-05 23:21:43 -0500

    CRUD

--- dist/resolvers/fridge.js
+++ dist/resolvers/fridge.js
@@ -11,6 +11,15 @@ var __metadata = (this && this.__metadata) || function (k, v) {
 var __param = (this && this.__param) || function (paramIndex, decorator) {
     return function (target, key) { decorator(target, key, paramIndex); }
 };
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.FridgeResolver = void 0;
 const Fridge_1 = require("../entities/Fridge");
@@ -21,6 +30,37 @@ let FridgeResolver = class FridgeResolver {
     }
     fridge(id, { em }) {
         return em.findOne(Fridge_1.Fridge, { id });
+    }
+    createFridge(title, { em }) {
+        return __awaiter(this, void 0, void 0, function* () {
+            const fridge = em.create(Fridge_1.Fridge, { title });
+            yield em.persistAndFlush(fridge);
+            return fridge;
+        });
+    }
+    updateFridge(id, title, { em }) {
+        return __awaiter(this, void 0, void 0, function* () {
+            const fridge = yield em.findOne(Fridge_1.Fridge, { id });
+            if (!fridge) {
+                return null;
+            }
+            if (typeof title !== 'undefined') {
+                fridge.title = title;
+                yield em.persistAndFlush(fridge);
+            }
+            return fridge;
+        });
+    }
+    deleteFridge(id, { em }) {
+        return __awaiter(this, void 0, void 0, function* () {
+            try {
+                yield em.nativeDelete(Fridge_1.Fridge, { id });
+            }
+            catch (_a) {
+                return false;
+            }
+            return true;
+        });
     }
 };
 __decorate([
@@ -38,6 +78,31 @@ __decorate([
     __metadata("design:paramtypes", [Number, Object]),
     __metadata("design:returntype", Promise)
 ], FridgeResolver.prototype, "fridge", null);
+__decorate([
+    type_graphql_1.Mutation(() => Fridge_1.Fridge),
+    __param(0, type_graphql_1.Arg('title')),
+    __param(1, type_graphql_1.Ctx()),
+    __metadata("design:type", Function),
+    __metadata("design:paramtypes", [String, Object]),
+    __metadata("design:returntype", Promise)
+], FridgeResolver.prototype, "createFridge", null);
+__decorate([
+    type_graphql_1.Mutation(() => Fridge_1.Fridge, { nullable: true }),
+    __param(0, type_graphql_1.Arg('id')),
+    __param(1, type_graphql_1.Arg('title', () => String, { nullable: true })),
+    __param(2, type_graphql_1.Ctx()),
+    __metadata("design:type", Function),
+    __metadata("design:paramtypes", [Number, String, Object]),
+    __metadata("design:returntype", Promise)
+], FridgeResolver.prototype, "updateFridge", null);
+__decorate([
+    type_graphql_1.Mutation(() => Fridge_1.Fridge, { nullable: true }),
+    __param(0, type_graphql_1.Arg('id')),
+    __param(1, type_graphql_1.Ctx()),
+    __metadata("design:type", Function),
+    __metadata("design:paramtypes", [Number, Object]),
+    __metadata("design:returntype", Promise)
+], FridgeResolver.prototype, "deleteFridge", null);
 FridgeResolver = __decorate([
     type_graphql_1.Resolver()
 ], FridgeResolver);
--- src/resolvers/fridge.ts
+++ src/resolvers/fridge.ts
@@ -1,6 +1,6 @@
 import { Fridge } from "../entities/Fridge";
 import { MyContext } from "src/types";
-import { Resolver, Query, Ctx, Arg, Int } from "type-graphql";
+import { Resolver, Query, Ctx, Arg, Int, Mutation } from "type-graphql";
 
 @Resolver()
 export class FridgeResolver {
@@ -15,5 +15,42 @@ export class FridgeResolver {
     @Ctx() { em }: MyContext
     ): Promise<Fridge | null> {
     return em.findOne(Fridge, { id });
+  }
+  @Mutation(() => Fridge)
+  async createFridge(
+    @Arg('title') title: string,
+    @Ctx() { em }: MyContext
+  ): Promise<Fridge> {
+    const fridge = em.create(Fridge, {title})
+    await em.persistAndFlush(fridge)
+    return fridge
+  }
+  @Mutation(() => Fridge, {nullable: true})
+  async updateFridge(
+    @Arg('id') id: number,
+    @Arg('title', () => String, {nullable: true}) title: string,
+    @Ctx() { em }: MyContext
+  ): Promise<Fridge | null> {
+    const fridge = await em.findOne(Fridge, { id })
+      if (!fridge) {
+        return null
+      }
+      if (typeof title !== 'undefined') {
+        fridge.title = title
+        await em.persistAndFlush(fridge)
+    }
+    return fridge
+  }
+  @Mutation(() => Fridge, {nullable: true})
+  async deleteFridge(
+    @Arg('id') id: number,
+    @Ctx() { em }: MyContext
+  ): Promise<boolean> {
+  try {
+    await em.nativeDelete(Fridge, { id })
+  } catch {
+    return false
+  }
+    return true
   }
 }
