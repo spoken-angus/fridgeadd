5c6254d87454e3b2b723be238c592c7d8e422ec1
spoken-angus <angusjelinek@gmail.com>
2020-11-05 00:02:14 -0600

    update backend for logout

--- api/src/resolvers/user.ts
+++ api/src/resolvers/user.ts
@@ -2,6 +2,7 @@ import { Resolver, Mutation, Arg, InputType, Field, Ctx, ObjectType, Query, } fr
 import { MyContext } from "../types";
 import { User } from "../entities/User";
 import argon2 from "argon2";
+import {COOKIE_NAME} from "../constants"
 
 @InputType()
 class UsernamePasswordInput {
@@ -122,5 +123,20 @@ message: 'password must be at least 7 character'
     return {
       user,
     };
+  }
+  @Mutation(() => Boolean)
+  logout(@Ctx() { req, res }: MyContext) {
+    return new Promise((resolve) =>
+      req.session.destroy((err) => {
+        res.clearCookie(COOKIE_NAME);
+        if (err) {
+          console.log(err);
+          resolve(false);
+          return;
+        }
+
+        resolve(true);
+      })
+    );
   }
 }
